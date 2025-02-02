50ee9d2ff47029beb7836682638f178d11973e2d
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2025-02-03 00:02:18 +0300

    refactor: remove unusual types

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,9 +2,7 @@ import { Component } from 'react';
 import Spin from '../Spin/Spin.tsx';
 import styles from './Loader.module.css';
 
-type SpinProps = object;
-type SpinState = object;
-class Loader extends Component<SpinProps, SpinState> {
+class Loader extends Component<object, object> {
   render = () => {
     return (
       <div className={styles.loader}>
